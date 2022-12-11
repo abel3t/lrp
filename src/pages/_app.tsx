b28ae623fbd600bf5247356ac74a9a186ac84d71
Abel Tran <abeltran.develop@gmail.com>
2022-12-11 17:26:28 +0700

    fix: update meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -118,12 +118,12 @@ const App = (props: ExtendedAppProps) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+        <title>{`${themeConfig.templateName} - LRP`}</title>
         <meta
           name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
+          content={`${themeConfig.templateName} – LRP.`}
         />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <meta name='keywords' content='LRP' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
