13513b464531d5ff413bfe8e12328627d06735cb
Abel Tran <abeltran.develop@gmail.com>
2022-12-17 09:15:31 +0700

    fix: env

--- src/@core/services/api.client.ts
+++ src/@core/services/api.client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
