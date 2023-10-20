4c26ae3a274d1e73669ae77055b1f3801350895a
Abel Tran <abeltran.develop@gmail.com>
2023-10-20 23:32:56 +0700

    delete un-use module

--- src/@core/components/repeater/types.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentType, ReactNode } from 'react';
-
-export type RepeaterProps = {
-  count: number;
-  children(i: number): ReactNode;
-  tag?: ComponentType | keyof JSX.IntrinsicElements;
-};
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,5 @@
 import Head from 'next/head';
-
 import Grid from '@mui/material/Grid';
-
-import KeenSliderWrapper from '@core/styles/libs/keen-slider';
-import RechartsWrapper from '@core/styles/libs/recharts';
 
 import NeedingMoreCare from './components/NeedingMoreCare';
 import Overview from './components/Overview';
@@ -16,35 +12,32 @@ const Dashboard = () => {
   const ability = useContext(AbilityContext);
 
   return (
-    <RechartsWrapper>
+    <>
       <Head>
         <title>Dashboard - Lighthouse Resource Planning</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-
-      <KeenSliderWrapper>
-        <Grid container spacing={6} className='match-height'>
-          <Grid item xs={12} md={6}>
-            <Overview />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <SundayServicePresence />
-          </Grid>
-
-          {
-            ability?.can('read', 'cares-overview') &&
-            <>
-              <Grid item xs={12} md={6}>
-                <NeedingMoreCare />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <TopCaringPeople />
-              </Grid>
-            </>
-          }
+      <Grid container spacing={6} className='match-height'>
+        <Grid item xs={12} md={6}>
+          <Overview />
         </Grid>
-      </KeenSliderWrapper>
-    </RechartsWrapper>
+        <Grid item xs={12} sm={12} md={6}>
+          <SundayServicePresence />
+        </Grid>
+
+        {
+          ability?.can('read', 'cares-overview') &&
+          <>
+            <Grid item xs={12} md={6}>
+              <NeedingMoreCare />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TopCaringPeople />
+            </Grid>
+          </>
+        }
+      </Grid>
+    </>
   );
 };
 
